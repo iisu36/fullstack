0c7eef38ac05786fd04381190bfeeb6b36179908
Iisakki Kulhua <iisakki@kulhua.net>
2022-04-10 09:53:02 +0300

    Oikea moduulirakenne ja suojauspäivitykset

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,83 @@
+const Header = ({ course }) => {
+
+    console.log('Header')
+    console.log(course)
+
+    return (
+        <div>
+            <h1>{course}</h1>
+        </div>
+    )
+}
+
+const Content = ({ parts }) => {
+
+    console.log('Content')
+    console.log(parts)
+
+    return (
+        <div>
+            {parts.map(part =>
+                <Part part={part} key={part.id} />
+            )}
+        </div>
+    )
+}
+
+const Part = ({ part }) => {
+
+    console.log('Part')
+    console.log(part)
+
+    return (
+        <>
+            <p>{part.name} {part.exercises}</p>
+        </>
+    )
+}
+
+const Total = ({ parts }) => {
+
+    console.log('Total')
+    console.log(parts)
+
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+    return (
+        <div>
+            <p style={{ fontWeight: 'bolder' }} >
+                total of {total} exercises
+            </p>
+        </div>
+    )
+}
+
+const Course = ({ course }) => {
+
+    return (
+        <div>
+            <Header course={course.name} />
+
+            <Content parts={course.parts} />
+
+            <Total parts={course.parts} />
+
+        </div>
+    )
+
+}
+
+const App = ({courses}) => {
+
+    return (
+        <div>
+
+            {courses.map(course =>
+                <Course course={course} key={course.id} />
+            )}
+
+        </div>
+    )
+}
+
+export default App
