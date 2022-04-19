4b63ea71b36999d370f574efca85c5021f8a88b5
Iisakki Kulhua <iisakki@kulhua.net>
2022-04-19 20:51:55 +0300

    5.9

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -20,15 +20,16 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
     
     const user = request.user
     
-    const blog = new Blog({
+    const blog = {
         title: body.title,
         author: body.author,
         url: body.url,
         likes: body.likes === undefined ? 0 : body.likes,
         user: user.id
-    })
+    }
 
-    const savedBlog = await blog.save()    
+    const savedBlog = await Blog.create(blog)
+    await savedBlog.populate('user', { username: 1, name: 1 })
     
     user.blogs = user.blogs.concat(savedBlog.id)
     
@@ -40,21 +41,36 @@ blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
     }
     
     const userUpdated = await User.findByIdAndUpdate(user.id, userToUpdate, { new: true, runValidators: true, context: 'query' })
+                        .populate('blogs', { title: 1, author: 1, likes: 1 })
     
     response.status(201).json(savedBlog.toJSON())
 })
 
-blogsRouter.put('/:id', async (request, response) => {
+blogsRouter.put('/:id', middleware.userExtractor, async (request, response) => {
     const body = request.body
+    const user = request.user
 
     const blog = {
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes === undefined ? 0 : body.likes
+        likes: body.likes === undefined ? 0 : body.likes,
+        user: user
     }
 
     const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true, runValidators: true, context: 'query' })
+                        .populate('user', { username: 1, name: 1 })
+    
+    const userToUpdate = {
+        username: user.username,
+        user: user.user,
+        passwordHash: user.passwordHash,
+        blogs: user.blogs.map(blog => blog.id !== updatedBlog.id ? blog : updatedBlog)
+    }
+    
+    const userUpdated = await User.findByIdAndUpdate(user.id, userToUpdate, { new: true, runValidators: true, context: 'query' })
+                        .populate('blogs', { title: 1, author: 1, likes: 1 })
+
     response.json(updatedBlog.toJSON())
 })
 
